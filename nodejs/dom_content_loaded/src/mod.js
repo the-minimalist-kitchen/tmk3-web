7ba2140ab7b77ca48a599b7b239e7b9c2f35134e
taylor <brian.t.vann@gmail.com>
2024-04-02 15:08:09 -0700

    map functionality to dom methods

--- nodejs/dom_content_loaded/src/mod.js
+++ nodejs/dom_content_loaded/src/mod.js
@@ -1,8 +1,8 @@
-// load service worker at www/
-navigator.serviceWorker.register('/sw.js')
-	.then(reg => console.log('registered /sw.js', reg))
-	// chain progressive feature load
-	// can be intercepted through the sworker relative to url
-	.catch(err => console.log('failed to register /sw.js', err));
-
-
+addEventListener("DOMContentLoaded", () => {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => console.log("registered /sw.js", reg))
+    // chain progressive feature load
+    // then load resources
+    .catch((err) => console.log("failed to register /sw.js", err));
+});
